4ac703c6fc58ee5e385304321d6f8534add8605e
Shugo Maeda <shugo@ruby-lang.org>
2018-10-23 11:59:40 +0900

    Add sample code for menus.

---
+++ sample/menu.rb
@@ -0,0 +1,29 @@
+require "curses"
+
+Curses.init_screen
+Curses.cbreak
+Curses.noecho
+Curses.stdscr.keypad = true
+at_exit do
+  Curses.cloes_screen
+end
+
+menu = Curses::Menu.new([
+  Curses::Item.new("Apple", "Red fruit"),
+  Curses::Item.new("Orange", "Orange fruit"),
+  Curses::Item.new("Banana", "Yello fruit")
+])
+menu.post
+
+while ch = Curses.getch
+  case ch
+  when Curses::KEY_UP, ?k
+    menu.driver(Curses::REQ_UP_ITEM)
+  when Curses::KEY_DOWN, ?j
+    menu.driver(Curses::REQ_DOWN_ITEM)
+  else
+    break
+  end
+end
+
+menu.unpost
