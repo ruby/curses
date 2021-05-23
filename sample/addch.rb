bea3c1837a4fdfcb110f3d1a4d18d09244b80d03
Christopher Oezbek <c.oezbek@gmail.com>
2021-05-24 00:57:52 +0200

    Add small sample on how to use addch to add a single character with an particular attribute.

---
+++ sample/addch.rb
@@ -0,0 +1,16 @@
+require_relative "../lib/curses"
+include Curses
+
+init_screen
+begin
+  addstr("The following letter A should be BOLD and UNDERLINED by using addch:\n")
+  addch('A'.ord | A_BOLD | A_UNDERLINE)
+
+  addstr("\nIt should look the same as when using attron and addstr:\n")
+  attron(A_BOLD | A_UNDERLINE)
+  addstr("A")
+  getch
+
+ensure
+  close_screen
+end
